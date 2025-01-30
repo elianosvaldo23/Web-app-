e5882ea143359d53f5af44c07b98070eb4d23c88
Tim <tymmesyde@gmail.com>
2025-01-30 11:10:45 +0100

    refactor(App): update updater banner style

--- src/App/UpdaterBanner/UpdaterBanner.tsx
+++ src/App/UpdaterBanner/UpdaterBanner.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react';
+import Icon from '@stremio/stremio-icons/react';
 import { useServices } from 'stremio/services';
 import { useBinaryState } from 'stremio/common';
 import { Button, Transition } from 'stremio/components';
 import styles from './UpdaterBanner.less';
-import classNames from 'classnames';
 
 type Props = {
     className: string,
@@ -32,14 +32,12 @@ const UpdaterBanner = ({ className }: Props) => {
                     <div className={styles['label']}>
                         A new version of Stremio is available
                     </div>
-                    <div className={styles['buttons']}>
-                        <Button className={styles['button']} onClick={hide}>
-                            Dismiss
-                        </Button>
-                        <Button className={classNames(styles['button'], styles['suggested'])} onClick={onInstallClick}>
-                            Install now
-                        </Button>
-                    </div>
+                    <Button className={styles['button']} onClick={onInstallClick}>
+                        Install now
+                    </Button>
+                    <Button className={styles['close']} onClick={hide}>
+                        <Icon className={styles['icon']} name={'close'} />
+                    </Button>
                 </div>
             </Transition>
         </div>
