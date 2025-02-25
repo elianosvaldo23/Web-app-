2b5df908271d23f8b690c3298c812afe44fc6b30
Botzy <bodim93@gmail.com>
2025-02-25 11:40:34 +0200

    feat(useWindowSize): added hook for screen size

--- src/common/index.js
+++ src/common/index.js
@@ -24,6 +24,7 @@ const { default: useShell } = require('./useShell');
 const useStreamingServer = require('./useStreamingServer');
 const useTorrent = require('./useTorrent');
 const useTranslate = require('./useTranslate');
+const useWindowSize = require('./useWindowSize');
 
 module.exports = {
     FileDropProvider,
@@ -55,4 +56,5 @@ module.exports = {
     useStreamingServer,
     useTorrent,
     useTranslate,
+    useWindowSize,
 };
