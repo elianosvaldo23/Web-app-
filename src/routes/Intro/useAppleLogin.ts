57d16957f2cfd483f9fb6f4651153d2d0ab57230
Timothy Z. <timothy@stremio.com>
2025-04-14 23:26:04 +0300

    chore(useapplelogin): update redirect to prod

--- src/routes/Intro/useAppleLogin.ts
+++ src/routes/Intro/useAppleLogin.ts
@@ -47,7 +47,7 @@ const useAppleLogin = (): [() => Promise<AppleLoginResponse>, () => void] => {
             window.AppleID.auth.init({
                 clientId: CLIENT_ID,
                 scope: 'name email',
-                redirectURI: 'https://stremio.github.io/stremio-web/feat/example-apple-login/',
+                redirectURI: 'https://web.stremio.com/',
                 state: 'signin',
                 usePopup: true,
             });
