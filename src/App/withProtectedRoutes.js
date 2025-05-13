8ca47aca9254ad05c388e862c99e6b6a29365c64
Botzy <bodim93@gmail.com>
2025-05-13 14:18:57 +0300

    feat(withProtectedRoutes): use navigate instead of window location

--- src/App/withProtectedRoutes.js
+++ src/App/withProtectedRoutes.js
@@ -3,20 +3,22 @@
 const React = require('react');
 const { Intro } = require('stremio/routes');
 const { useProfile } = require('stremio/common');
+const { useNavigate } = require('react-router');
 
 const withProtectedRoutes = (Component) => {
+    const navigate = useNavigate();
     return function withProtectedRoutes(props) {
         const profile = useProfile();
         const previousAuthRef = React.useRef(profile.auth);
         React.useEffect(() => {
             if (previousAuthRef.current !== null && profile.auth === null) {
-                window.location = '#/intro';
+                navigate('/intro');
             }
             previousAuthRef.current = profile.auth;
         }, [profile]);
         const onRouteChange = React.useCallback((routeConfig) => {
             if (profile.auth !== null && routeConfig.component === Intro) {
-                window.location.replace('#/');
+                navigate('/', { replace: true });
                 return true;
             }
         }, [profile]);
