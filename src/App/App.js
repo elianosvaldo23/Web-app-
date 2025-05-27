5d9a005686b838390ba6532bd3207d63d58773af
Tim <tymmesyde@gmail.com>
2025-05-27 20:29:42 +0200

    style(App): use singlequote for string

--- src/App/App.js
+++ src/App/App.js
@@ -113,7 +113,7 @@ const App = () => {
                     }
                 }
             } catch (e) {
-                console.error("Failed to open media:", e);
+                console.error('Failed to open media:', e);
             }
         };
 
